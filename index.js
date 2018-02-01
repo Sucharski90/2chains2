a6e9a3b246af2554b73bf9efebdce735929fa200
Matthew Sucharski <a7x1013@aol.com>
2018-02-01 06:30:46 -0500

    redoing css

--- index.js
+++ index.js
@@ -1,3 +1,57 @@
+$(window).load(function() {
+	let InfiniteRotator =
+	{
+		init: function()
+		{
+			let initialFadeIn = 1000;
+			let itemInterval = 5000;
+			let fadeTime = 2500;
+			let numberOfItems = $('.rotating-item').length;
+			let currentItem = 0;
+			$('.rotating-item').eq(currentItem).fadeIn(initialFadeIn);
+			let infiniteLoop = setInterval(function(){
+				$('.rotating-item').eq(currentItem).fadeOut(fadeTime);
+				if(currentItem == numberOfItems -1){
+					currentItem = 0;
+				}   else {
+				}{
+					currentItem++;
+				}
+				$('.rotating-item').eq(currentItem).fadeIn(fadeTime);
+			}, itemInterval);
+		}
+	};
+	InfiniteRotator.init();
+});
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+          // Store hash
+          var hash = this.hash;
+
+          // Using jQuery's animate() method to add smooth page scroll
+          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 800, function(){
+
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          });
+        } // End if
+      });
+    });
+
+
+
+
+
 //add sticky to header, taken off because it would mess up the image rotation
 // window.onscroll = function() {myFunction()};
 // let header = document.querySelector("header");
@@ -9,41 +63,3 @@
 //     header.classList.remove("sticky");
 //   }
 // }
-
-
-$(window).load(function() {
-	let InfiniteRotator =
-	{
-		init: function()
-		{
-			let initialFadeIn = 1000;
-
-			let itemInterval = 5000;
-
-			let fadeTime = 2500;
-
-			let numberOfItems = $('.rotating-item').length;
-
-			let currentItem = 0;
-
-			$('.rotating-item').eq(currentItem).fadeIn(initialFadeIn);
-
-			let infiniteLoop = setInterval(function(){
-				$('.rotating-item').eq(currentItem).fadeOut(fadeTime);
-
-				if(currentItem == numberOfItems -1){
-					currentItem = 0;
-				}   else {
-
-				}{
-					currentItem++;
-				}
-				$('.rotating-item').eq(currentItem).fadeIn(fadeTime);
-
-			}, itemInterval);
-		}
-	};
-
-	InfiniteRotator.init();
-
-});
